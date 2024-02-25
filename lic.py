b80fa6bd7eb88dfb1395cd871c61b4f0d53077dd
Dominik Wojnowski <dwojnow1@intercars.eu>
2024-02-25 09:13:26 +0100

    init

---
+++ lic.py
@@ -0,0 +1,30 @@
+def lic(l, r):
+    arr = None
+
+    potyczkowa_number = []
+    for i in range(l, r + 1):
+        arr = list(dict.fromkeys(list(str(i))))
+        # print(arr)
+        if '0' in arr:
+            continue
+
+        is_potyczkowa_number = True
+        for j in arr:
+            if j == '1':
+                continue
+            elif i % int(j) == 0:
+                continue
+            else:
+                is_potyczkowa_number = False
+                break
+        if is_potyczkowa_number == True:
+            potyczkowa_number.append(i)
+
+    # print(potyczkowa_number)
+    # print(arr)
+
+    return len(potyczkowa_number)
+
+
+if __name__ == '__main__':
+    print(lic(1000000, 100000000))
